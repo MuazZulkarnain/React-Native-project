d9227e6f7370c321d62bc8421fe2e74789b36745
Muaz Zul <idmuazzulkarnain@gmail.com>
2023-02-13 02:05:32 +0800

    Added some image in image screen

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,10 +1,21 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
-const ImageDetail = () => {
-  return <Text>This is Image Detail</Text>;
+const ImageDetail = (props) => {
+  console.log(props);
+  return (
+    <View>
+      <Image style={styles.img} source={props.imageSource} />
+      <Text>{props.title}</Text>
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  img: {
+    height: 200,
+    width: 200,
+  },
+});
 
 export default ImageDetail;
